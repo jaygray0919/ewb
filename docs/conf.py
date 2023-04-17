d5e63f538f444e095721084a1da8a1f8989404b7
jay gray <jaygray0919@gmail.com>
2023-04-17 18:25:49 -0500

    removed collapse

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,6 @@ CURDIR = os.path.abspath(os.path.dirname(__file__))
 extensions = [
     'sphinxcontrib.bibtex',
     'sphinxcontrib.contentui',
-    'sphinx_toolbox.collapse',
     'sphinx.ext.graphviz',
     ]
 
